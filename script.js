a58239669349b9dc4363326c628285cabe9e0714
olafmartinez <martineztorres1725@gmail.com>
2026-05-11 16:05:45 -0600

    <Cambios>(proyecto):

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function mostrarMensaje(){
   "martineztorres1725@gmail.com | +52 5525624519";
 
 }
+
+window.addEventListener("load", () => {
+
+  setTimeout(() => {
+
+    document.getElementById("splash").style.display = "none";
+
+  }, 3000);
+
+});
